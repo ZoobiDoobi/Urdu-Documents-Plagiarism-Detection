eb471cc38571487b5e44b0a177a251939223dd0e
ZoobiDoobi <bscsf12m033@gmail.com>
2016-05-09 07:00:45 +0500

    Added Synonym Replacement Class and a Little Dictionary

---
+++ UPlagSolution/AlgorithmModules/SynonymReplacement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace UPlagSolution.AlgorithmModules
+{
+    public class SynonymReplacement
+    {
+        public string[] ContentToBeSynonymReplaced { get; set; }
+
+        public Dictionary<string,string> UrduDictionary { get; set; }
+
+        public SynonymReplacement(string[] content)
+        {
+            ContentToBeSynonymReplaced = content;
+            LoadDictionary();
+        }
+
+        private void LoadDictionary()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), @"Dictionary\Dictionary.txt");
+            var lines = File.ReadAllLines(path);
+            foreach (var item in lines)
+            {
+                string[] temp = item.Split(':'); // each time a line is read from dictionary, declare a new string array
+                UrduDictionary.Add(temp[0], temp[1]);
+            }
+        }
+
+        public void PerformSynonymReplacemnt()
+        {
+            
+            for (int i = 0; i < ContentToBeSynonymReplaced.Length; i++)
+            {
+                if (UrduDictionary.ContainsKey(ContentToBeSynonymReplaced[i]))
+                {
+                    ContentToBeSynonymReplaced[i] = UrduDictionary.Where(x => x.Key == ContentToBeSynonymReplaced[i]).Select(p => p.Value).FirstOrDefault();
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
